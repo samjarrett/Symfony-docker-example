a67647a0ed8fa46c8224887124131e52c066c875
Sam Jarrett <samstah@gmail.com>
2017-07-29 19:15:42 +1000

    Lastly, before trying to boot, wait up to 2mins for MySQL to come up and be ready to accept connections

--- config/php-fpm/docker-entrypoint.sh
+++ config/php-fpm/docker-entrypoint.sh
@@ -7,6 +7,8 @@ if [ "$1" == php-fpm ]; then
         composer dump-autoload
     fi
 
+    dockerize -wait tcp://${DATABASE_HOST}:${DATABASE_PORT:-3306} -timeout 120s
+
     su -s /bin/sh \
        -c "bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration" \
        www-data
