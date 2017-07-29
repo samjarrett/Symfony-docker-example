6ac7f776440809c75f115e5667591a0ecc02ea33
Sam Jarrett <samstah@gmail.com>
2017-07-29 19:13:11 +1000

    When we're running in dev, re-generate the composer autoload conf to not be authoritative, so we can dynamically add classes without autoload errors

--- config/php-fpm/docker-entrypoint.sh
+++ config/php-fpm/docker-entrypoint.sh
@@ -3,6 +3,10 @@
 if [ "$1" == php-fpm ]; then
     set -e
 
+    if [ "$SYMFONY_ENV" == "dev" ]; then
+        composer dump-autoload
+    fi
+
     su -s /bin/sh \
        -c "bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration" \
        www-data
